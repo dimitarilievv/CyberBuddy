c58b38ffcea42e6ed5a75ec926bbd6e03c822425
damjankaradakoski <damjankaradakoski77@gmail.com>
2026-05-04 12:50:27 +0200

    CYB-53- Dockerization and test deployment settings p2

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'], // adjust to your inputs
+            input: ['resources/css/app.css', 'resources/js/app.js'], // adjust to your inputs
             refresh: true,
         }),
     ],
